b36b09b15df0640ea2513ae823b72dac3bf86c1a
zozgeyilmaz <zubeydeozgeyilmaz@gmail.com>
2021-10-19 00:04:01 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,28 @@
+console.log("Merhaba Kodlama.io")
+
+var dolarDun = 9.20
+let dolarBugun = 9.30
+
+{
+    var dolarDun = 9.10
+    //console.log(dolarDun)
+}
+console.log(dolarDun)
+
+const euroDun = 11.2
+//euroDun = 11 
+
+console.log(euroDun)
+//array
+//camelCasing
+//PascalCasing
+let konutKredileri = ["Konut Kredisi",
+"Emlak Konur Kredisi",
+"Kamu Konut Kredisi",
+"Özel Konut Kredisi"]
+console.log("<ul>")
+for (let index = 0; index < konutKredileri.length; index++) {
+    console.log("   <li>"+konutKredileri[index]+"</li>")
+}
+console.log("</ul>")
+
